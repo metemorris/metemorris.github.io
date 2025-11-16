1112e1dd9764d91629716c6bc842ecc9fa784893
Mete Morris <metemorris@Mac.localdomain>
2025-11-16 11:31:43 -0800

    improve tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,8 @@ const posts = defineCollection({
     title: z.string(),
     date: z.date(),
     description: z.string().max(160),
+    seoTitle: z.string().min(50).max(60),
+    seoDescription: z.string().min(120).max(160),
     tags: z.array(z.string()).default([]),
     draft: z.boolean().default(false),
   }),
