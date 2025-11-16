dfb40810a00e9fa05ca035a87f406e7bed2f2ec6
Mete Morris <metemorris@Mac.localdomain>
2025-11-16 11:15:05 -0800

    fix rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,42 @@
-import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
-  const posts = await getCollection('posts');
+const asCdata = (value) => `<![CDATA[${value.replaceAll(']]>', ']]]]><![CDATA[>')}]]>`;
 
-  return rss({
-    title: 'Mete Morris Blog',
-    description: 'Latest posts from Mete Morris',
-    site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      description: post.data.description,
-      link: `/posts/${post.slug}/`,
-      pubDate: post.data.date,
-    })),
+export async function GET(context) {
+  const site = context.site ?? 'https://www.metemorris.com';
+  const siteUrl = new URL(site);
+  const posts = await getCollection('posts');
+  const publishedPosts = posts
+    .filter((post) => !post.data.draft)
+    .sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
+
+  const items = publishedPosts
+    .map((post) => {
+      const postUrl = new URL(`/posts/${post.slug}/`, siteUrl).href;
+      return `    <item>
+      <title>${asCdata(post.data.title)}</title>
+      <link>${postUrl}</link>
+      <guid>${postUrl}</guid>
+      <description>${asCdata(post.data.description)}</description>
+      <pubDate>${post.data.date.toUTCString()}</pubDate>
+    </item>`;
+    })
+    .join('\n');
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0">
+  <channel>
+    <title>${asCdata('Mete Morris Blog')}</title>
+    <link>${siteUrl.href}</link>
+    <description>${asCdata('Latest posts from Mete Morris')}</description>
+    <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+${items}
+  </channel>
+</rss>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/rss+xml; charset=utf-8',
+    },
   });
 }
